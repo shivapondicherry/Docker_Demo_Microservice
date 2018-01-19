5e380205cfac2fb5b6ef9e387afa0373f9c5926e
shivapondicherry <34459942+shivapondicherry@users.noreply.github.com>
2018-01-19 13:21:17 +0530

    Delete SidedMarketDataListener.java

--- src/main/java/net/openhft/samples/microservices/orders/SidedMarketDataListener.java
+++
@@ -1,8 +0,0 @@
-package net.openhft.samples.microservices.orders;
-
-/**
- * Created by peter on 22/03/16.
- */
-public interface SidedMarketDataListener {
-    void onSidedPrice(SidedPrice sidedPrice);
-}
