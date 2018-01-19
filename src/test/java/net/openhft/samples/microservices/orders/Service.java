75ced40c11fcdb59e6c14f400007d68df4869d14
siva <shivapondicherry@gmail.com>
2018-01-19 14:10:09 +0530

    delete orders

--- src/test/java/net/openhft/samples/microservices/orders/Service.java
+++
@@ -1,8 +0,0 @@
-package net.openhft.samples.microservices.orders;
-
-/**
- * Created by peter on 02/04/16.
- */
-interface Service {
-    void simpleCall(SimpleData data);
-}
--- src/test/java/net/openhft/samples/microservices/orders/ServiceHandler.java
+++
@@ -1,8 +0,0 @@
-package net.openhft.samples.microservices.orders;
-
-/**
- * Created by peter on 01/04/16.
- */
-public interface ServiceHandler<O> {
-    void init(O output);
-}
